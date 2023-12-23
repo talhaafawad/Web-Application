8c6aed09d073de5abc7770a17cc9410a71080489
talhaafawad <124430467+talhaafawad@users.noreply.github.com>
2023-12-23 10:07:32 +0000

    Add files via upload
    🌐 Built a dynamic Joke Web App using .NET Core MVC, Entity Framework, C#, HTML, CSS, and Bootstrap. This application enables users to create, search, and enjoy jokes via a seamless interface developed with HTML, CSS, and Bootstrap.
    
    🔐 Implemented user authentication and streamlined data management using Entity Framework, showcasing proficiency in .NET Core technologies for responsive and engaging web development.
    
    🚀 Explore the repository to delve into the code and witness firsthand the integration of these technologies into an interactive and functional web application.

---
+++ ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace New.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
